c6ae83e3c4e18de7b97d950f9f8a00337eed7eca
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2023-07-12 19:07:05 +0200

    finish section on male and female suicide

--- Coding/load_data.py
+++ Coding/load_data.py
@@ -3,24 +3,25 @@ import pandas as pd
 import csv
 import os
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
 def load_file(path):
     df = pd.read_csv(path, encoding="utf-8")
     return df
 
-def plot_genders_mean_suicide_rate(df):
+def plot_genders_mean_suicide_rate(df, save_to_pdf = False):
 
     # Filter the data to only include male and female suicide rates    
-    df_males = df.loc[df["Sex"].str.contains("Male")]
-    df_females = df.loc[df["Sex"].str.contains("Female")]
+    df_males, df_females = get_male_and_female_suicide_rates(df)
     
-    # print(df_males.head())
-
     plt.bar(["Muži", "Ženy"], [df_males["2016"].mean(), df_females["2016"].mean()], color = ["blue", "pink"])
     plt.xlabel("Pohlaví")
     plt.ylabel("Průměrný počet sebevražd na 100k obyvatel")
     plt.title("Průměrný počet sebevražd dle pohlaví")
+
+    if save_to_pdf == True:
+        plt.savefig("genders_mean_suicide_rate.pdf")
     plt.show()
     
 
@@ -29,12 +30,54 @@ def list_countries_where_women_kill_them_self_more(df):
     pivot.columns.name = None
     pivot = pivot.reset_index()
     women_more = pivot.loc[pivot[" Female"] > pivot[" Male"]]
-    print(women_more)
     
+    return women_more
+
+def year_2016_of_data_to_numpy(df_males, df_females):
+    return df_males["2016"].to_numpy(), df_females["2016"].to_numpy()
+
+
+def get_male_and_female_suicide_rates(df):
+    df_males = df.loc[df["Sex"].str.contains("Male")]
+    df_females = df.loc[df["Sex"].str.contains("Female")]
+    return df_males, df_females
+
+def two_sample_t_test_of_suicide_rates(df):
+    df_males, df_females = get_male_and_female_suicide_rates(df)
+    np_males, np_females = year_2016_of_data_to_numpy(df_males, df_females)
+
+    t_statistic, p_value = stats.ttest_ind(np_males, np_females, equal_var = False)
+    return(t_statistic, p_value)
+
+def Levene_test_male_and_female_suicide_rate_variance(df):
+    df_males, df_females = get_male_and_female_suicide_rates(df)
+    np_males, np_females = year_2016_of_data_to_numpy(df_males, df_females)
+    
+    levene = stats.levene(np_males, np_females)
+    return levene
+
+def Shapiro_test(df):
+    df_males, df_females = get_male_and_female_suicide_rates(df)
+    np_males, np_females = year_2016_of_data_to_numpy(df_males, df_females)
+
+    print(stats.shapiro(np_males), stats.shapiro(np_females))
+
+def histogram(df):
+    df_males, df_females = get_male_and_female_suicide_rates(df)
+    np_males, np_females = year_2016_of_data_to_numpy(df_males, df_females)
+
+    plt.hist(np_males, bins=20)
+    plt.hist(np_females, bins=20)
+    plt.show()
+
+
 if __name__ == "__main__":
     df = load_file("data/Age-standardized suicide rates.csv")
     print(df.head())
     print(" ")
     # print(df.dtypes)
-    plot_genders_mean_suicide_rate(df)
-    list_countries_where_women_kill_them_self_more(df)
+    # plot_genders_mean_suicide_rate(df, save_to_pdf = True)
+    print(list_countries_where_women_kill_them_self_more(df))
+    histogram(df)
+    # print(two_sample_t_test_of_suicide_rates(df))
+    # print(Levene_test_male_and_female_suicide_rate_variance(df))
